ac5f543421783cc4e322a2f0f23930b387a12a72
Natalia Bagan <natbagan@gmail.com>
2015-04-01 17:08:59 -0400

    Almost Final

--- www/js/view.js
+++ www/js/view.js
@@ -15,39 +15,39 @@ var app = function(app) {
 		
 		p.main = new createjs.Container();
 		p.main.name = "main";
-		p.main.setBounds(0, 0, stageW, stageH);
+		//p.main.setBounds(0,0,stageW,stageH);
 		
-		var content = new createjs.Container();
-		content.setBounds(0,0,360,640);
+		var content = p.main.content = new createjs.Container();
+		content.setBounds(0,0,360,600);
 		p.main.addChild(content);
 
 		var title = new createjs.Bitmap(preload.getResult("logo"));
-		title.x = 120;
+		title.x = 110;
 		title.y = 100;
 		content.addChild(title);
 		
 		var house = new createjs.Bitmap(preload.getResult("house"));
-		house.x = 60;
+		house.x = 50;
 		house.y = 100;
 		content.addChild(house);
 		
 		var paw = new createjs.Bitmap(preload.getResult("paw"));
-		paw.x = 70;
+		paw.x = -10;
 		paw.y = 200;
 		content.addChild(paw);
 		
 		var headline = new createjs.Bitmap(preload.getResult("headline"));
-		headline.x = 130;
-		headline.y = 420;
+		headline.x = 100;
+		headline.y = 550;
 		content.addChild(headline);
 				
 		//percentages
 		var mainParts = [
-			{object: content, marginLeft: 0, maxHeight: 100, width: 100, valign: "top"},
-			{object: house, marginLeft: 10, maxHeight: 100, width: 100, valign: "top"},
-			{object: title, marginTop:0, maxWidth:100, minHeight:100, align:"middle", valign:"top"},
-			{object: paw, marginTop:0, maxWidth:100, minHeight:100, align:"middle", valign:"top"},
-			{object: headline, marginTop:0, maxWidth:100, minHeight:100, align:"middle", valign:"top"}	
+			{object: content, marginLeft: 5, maxHeight: 100, width: 100, valign: "top"}
+			//{object: house, marginLeft: 10, maxHeight: 100, width: 100, valign: "top"},
+			//{object: title, marginTop:0, maxWidth:100, minHeight:100, align:"middle", valign:"top"},
+			//{object: paw, marginTop:0, maxWidth:100, minHeight:100, align:"middle", valign:"top"},
+			//{object: headline, marginTop:0, maxWidth:100, minHeight:100, align:"middle", valign:"top"}	
 		];
 		
 		//function(holder, regions, lastMargin, backgroundColor, vertical, regionShape, scalingObject, hideKey)
@@ -60,21 +60,29 @@ var app = function(app) {
 		p.info = new createjs.Container();
 		p.info.name = "info";
 		
-		var infoBacking = new zim.Rectangle(stageW, stageH, "#FFF");
-		infoBacking.setBounds(0, 0, stageW, stageH);
-		p.info.addChild(infoBacking);
+		var content2 = p.info.content2 = new createjs.Container();
+		content2.setBounds(0,0,360,600);
+		p.info.addChild(content2);
 		
-		var title = new createjs.Text("page2", "20px Arial", "#333333");
-		title.setBounds(null, null, 100, 100);
-		title.x = 100;
-		title.y= 100;
- 		title.textBaseline = "alphabetic";
-		infoBacking.addChild(title);
+		var barTop = new zim.Rectangle(450, 70, "#222");
+		barTop.x = -20;
+		barTop.y = 0;
+		content2.addChild(barTop);
 		
-		var pet = new createjs.Bitmap(preload.getResult("dog"));
-		pet.x = 200;
-		pet.y = 400;
-		infoBacking.addChild(pet);
+		var title2 = new createjs.Bitmap(preload.getResult("logo"));
+		title2.x = 70;
+		title2.y = 5;
+		content2.addChild(title2);
+		
+		var house2 = new createjs.Bitmap(preload.getResult("house"));
+		house2.x = 10;
+		house2.y = 5;
+		content2.addChild(house2);
+		
+		var pet = new createjs.Bitmap(preload.getResult("pet"));
+		pet.x = 80;
+		pet.y = 220;
+		content2.addChild(pet);
 		
 		zim.drag(pet);
 		
@@ -82,24 +90,30 @@ var app = function(app) {
 			console.log("moving");
 		});
 		
+		
+		var buttonBar = new zim.Rectangle(450, 170, "#ccc");
+		buttonBar.x = -20;
+		buttonBar.y = 560;
+		content2.addChild(buttonBar);
+		
 		var love = new createjs.Bitmap(preload.getResult("love"));
-		love.x = 150;
-		love.y = 500;
-		infoBacking.addChild(love);
+		love.x = 80;
+		love.y = 565;
+		content2.addChild(love);
 		
 		var food = new createjs.Bitmap(preload.getResult("feed"));
-		food.x = 200;
-		food.y = 500;
-		infoBacking.addChild(food);
+		food.x = 190;
+		food.y = 565;
+		content2.addChild(food);
 		
 		var loveme = new createjs.Bitmap(preload.getResult("loveme"));
-		loveme.x = 200;
-		loveme.y = 200;
+		loveme.x = 140;
+		loveme.y = 115;
 		loveme.alpha = 0;
 
 		var feedme = new createjs.Bitmap(preload.getResult("feedme"));
-		feedme.x = 200;
-		feedme.y = 200;
+		feedme.x = 140;
+		feedme.y = 115;
 		feedme.alpha = 0;
 
 		love.addEventListener("click", handleClick);
@@ -107,8 +121,8 @@ var app = function(app) {
 			console.log("love me");
 			stage.addChild(loveme);
 			createjs.Tween.get(loveme).to({alpha: 1},500)
-			.wait(500)
-			.to({alpha:0}, 500);
+			.wait(300)
+			.to({scaleX: 1.5, scaleY: 1.5, alpha:0}, 500);
 			createjs.Ticker.addEventListener("tick", stage);
 			stage.removeChild(feedme);
 		}
@@ -118,8 +132,8 @@ var app = function(app) {
 			console.log("feed me");
 			stage.addChild(feedme);
 			createjs.Tween.get(feedme).to({alpha:1},500)
-			.wait(500)
-			.to({alpha:0}, 500);
+			.wait(300)
+			.to({scaleX: 1.5, scaleY: 1.5, alpha:0}, 500);
 			createjs.Ticker.addEventListener("tick", stage);
 			stage.removeChild(loveme);
 		}
@@ -133,13 +147,10 @@ var app = function(app) {
 		}*/
 		
 		var petPage = [ 
-			{object: title, marginLeft:0, maxHeight: 40, width: 40, valign:"bottom"},
-			{object: pet, marginLeft:0, maxHeight: 40, width: 40, valign:"bottom"},
-			{object: love, marginLeft:0, maxHeight: 40, width: 40, valign:"bottom"},
-			{object: food, marginLeft:0, maxHeight: 40, width: 40, valign:"bottom"}
+			{object: content2, marginLeft: 5, maxHeight: 100, width: 100, valign: "top"}
 		];
 		
-		var secondLayout = new zim.Layout(p.main, petPage, 10, "#FFF", true, null, stage);
+		var secondLayout = new zim.Layout(p.main, petPage, 10, "#ccc", true, null, stage);
 
 		layoutManager.add(secondLayout);
 		
